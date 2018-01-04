2c04f1f951dfdd7873bd72bb41d75c90e0f9400e
Hanh Ha <tranhanh.haminh@gmail.com>
2018-01-04 10:53:22 +0700

    Adding option to disable telegram bot

--- acts_config.py
+++ acts_config.py
@@ -2,3 +2,5 @@ import configparser as CfgPsr
 
 config = CfgPsr.ConfigParser ()
 config.read ('acts_config.ini')
+
+telegram_en = True if 'Telegram' in config.keys() else False
--- speaker_agent.py
+++ speaker_agent.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-from telegram.ext import (Updater, CommandHandler, CallbackQueryHandler)
-from telegram.error import (TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError)
-
-import acts_bot as chatbot
-
 import acts_config as cfg
 
+if cfg.telegram_en:
+	import acts_bot as chatbot
+	
+def call_notify (text):
+	if cfg.telegram_en:
+		chatbot.notify (text)
+	else:
+		print (text)
+
+def call_ask_confirm (text):
+	if cfg.telegram_en:
+		chatbot.ask_confirm (text)
+		return chatbot.answerQueue.get (block = True)
+	else:
+		return True if raw_input (text) == 'yes' else False
+		
 def notf (inpQ, outQ, params, Stop):
 	if params[0] == 'rem':
 		fix_str = True
@@ -18,16 +29,16 @@ def notf (inpQ, outQ, params, Stop):
 		if not inpQ.empty():
 			info = inpQ.get (block = False)
 			if fix_str:
-				chatbot.notify (info_str)
+				call_notify (info_str)
 			else:
 				if type(info) is tuple:
 					if info[0] is True:
-						chatbot.notify (str (info[1][params[0]]))
+						call_notify (str (info[1][params[0]]))
 					else:
-						chatbot.notify (info[1])
+						call_notify (info[1])
 				else:
 					if info is True:
-						chatbot.notify (str(param[0]))
+						call_notify (str(param[0]))
 			inpQ.task_done ()
 
 def cfrm (inpQ, outQ, params, Stop):
@@ -36,18 +47,19 @@ def cfrm (inpQ, outQ, params, Stop):
 	while not Stop.is_set ():
 		if not inpQ.empty():
 			tmp = inpQ.get (block = False)
-			chatbot.ask_confirm (question)
-			ans = chatbot.answerQueue.get (block = True)
-			print ('User answered: {ans}'.format(ans=ans))
+			ans = call_ask_confirm (question)
+			#print ('User answered: {ans}'.format(ans=ans))
 			if ans is True:
 				for q in outQ:
 					q.put (True, block = True)
 			inpQ.task_done ()
 			
 def stop ():
-	chatbot.tbot.stop ()
+	if cfg.telegram_en:
+		chatbot.tbot.stop ()
 	
 def workable ():
-	return chatbot.tbot.workable
+	return chatbot.tbot.workable if cfg.telegram_en else True
 	
-chatbot.tbot.live ()
+if cfg.telegram_en:
+	chatbot.tbot.live ()
