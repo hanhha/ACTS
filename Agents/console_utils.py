6219be71bd7d4c0d43c52b22a70b0ff35f87dd61
Hanh Ha <tranhanh.haminh@gmail.com>
2018-01-19 22:24:19 +0700

    Trial and error

--- Agents/console_utils.py
+++ Agents/console_utils.py
@@ -2,7 +2,6 @@ import curses
 from threading import Lock, Event 
 
 class ConsoleScreen(object):
-
 	def __init__ (self):
 		self.screen = None
 		self.clsLock = Lock ()
@@ -19,17 +18,86 @@ class ConsoleScreen(object):
 		curses.noecho ()
 		curses.cbreak ()
 		self.screen.keypad (True)
-
 		self._Stop.clear ()
 
 	def end (self):
-		self.clsLock.acquire ()
-		self._Stop.set ()
-		self.clsLock.release ()
+		if self.enabled:
+			self._Stop.set ()
+			self.enabled = False
+			if self._execThread is not None:
+				self._execThread.join ()
 
 		self.screen.keypad (False)
 		curses.nocbreak ()
 		curses.echo ()
 		curses.endwin ()
 
+class AWindow(object):
+	def __init__(self, h, w, y, x, initial_content = None, bkgd = None, archive_limit = 20, **kwargs):
+		self._archive_limit = archive_limit
+		self._archive       = []
+		self._bkgd          = bkgd
 
+		self.create (h, w, y, x, bkgd)
+
+		if initial_content is not None:
+			self.addstr (initial_content, **kwargs)
+
+	def create (self, h, w, y, x, bkgd):
+		self._win = curses.newwin (h, w, y, x)
+
+		self._win.scrollok (True)
+		self._win.clear ()
+		self._win.idlok (True)
+
+		self.refresh ()
+
+		if bkgd is not None:
+			self.win.bkgd(bkgd)
+
+		self.refresh ()
+
+	@property
+	def win (self):
+		return self._win
+
+	def addstr (self, *args, **kwargs):
+		self._win.addstr (*args, **kwargs)
+		self.refresh ()
+		self._archive.append ((args, kwargs))
+		if len(self._archive) > self._archive_limit:
+			del self._archive [0]
+
+	def restore (self):
+		self._win.clear ()
+		for archive in self._archive:
+			self._win.addstr (*archive[0], **archive[1])
+			self.refresh ()
+
+		self.refresh ()
+
+	def refresh (self):
+		self._win.noutrefresh ()
+
+	def resize (self, h, w, y, x):
+		self.create (h, w, y, x, self._bkgd)
+		self.restore ()
+
+class ABoxWindow(AWindow):
+	def __init__(self, h, w, y, x, title = None, initial_content = None, bkgd = None, archive_limit = 20, **kwargs):
+		self._title = title
+
+		self.createBox (h, w, y, x, title = title)
+
+		AWindow.__init__ (self, h-2, w-2, y+1, x+1, initial_content = initial_content, bkgd = bkgd, archive_limit = archive_limit, **kwargs)
+
+	def createBox (self, h, w, y, x, title = None):		
+		self._WinBox = curses.newwin (h, w, y, x)
+		self._WinBox.box ()
+		if title is not None:
+			self._WinBox.addstr (0, 2, '[' + title + ']')
+		self._WinBox.noutrefresh ()
+
+	def resize (self, h, w, y, x):
+		self.createBox (h, w, y, x, title = self._title)
+		AWindow.resize (self, h - 2, w - 2, y + 1, x + 1)
--- Agents/user_interface.py
+++ Agents/user_interface.py
@@ -11,42 +11,6 @@ try:
 except (SystemError, ImportError):
 	import console_utils as cls
 
-class AWindow(object):
-	def __init__(self, h, w, y, x, initial_content = None, bkgd = None, archive_limit = 20, **kwargs):
-		self._win = curses.newwin (h, w, y, x)
-		
-		self._win.scrollok (True)
-		self._win.clear ()
-		self._win.idlok (True)
-		self._archive_limit = archive_limit
-		self._archive = []
-
-		#if bkgd is not None:
-		#	self.win.bkgd = bkgd
-
-		if initial_content is not None:
-			self.addstr (initial_content, **kwargs)
-
-	@property
-	def win (self):
-		return self._win
-
-	def addstr (self, *args, **kwargs):
-		self._win.addstr (*args, **kwargs)
-		self.refresh ()
-		self._archive.append ((args, kwargs))
-		if len(self._archive) > self._archive_limit:
-			del self._archive [0]
-
-	def restore (self):
-		self._win.clear ()
-		for archive in self._archive:
-			self._win.addstr (*archive[0], **archive[1])
-			self.refresh ()
-
-	def refresh (self):
-		self._win.noutrefresh ()
-
 
 class UserInterface(cls.ConsoleScreen):
 	def __init__ (self, title):
@@ -56,7 +20,7 @@ class UserInterface(cls.ConsoleScreen):
 		self.tipWin = None
 		self.evaWin = None
 		self.curWin = None
-		
+
 		self.maxX = None 
 		self.maxY = None 
 
@@ -65,9 +29,10 @@ class UserInterface(cls.ConsoleScreen):
 
 	def start (self):
 		cls.ConsoleScreen.start (self)
-
 		self.initConsole ()
+		self.run ()
 
+	def run (self):
 		def worker ():
 			while (not self._Stop.is_set()):
 				resized = curses.is_term_resized(self.maxY, self.maxX)
@@ -81,15 +46,11 @@ class UserInterface(cls.ConsoleScreen):
 
 		self.enabled = True
 
-	def end (self):
-		self.enabled = False
-		cls.ConsoleScreen.end (self)
-
 	def initConsole (self, refresh = False):
 		self.clsLock.acquire ()
 
 		self.maxY, self.maxX = self.screen.getmaxyx ()
-		
+
 		curses.start_color ()
 		curses.init_pair (1, curses.COLOR_GREEN , curses.COLOR_BLACK)
 		curses.init_pair (2, curses.COLOR_RED   , curses.COLOR_BLACK)
@@ -113,20 +74,11 @@ class UserInterface(cls.ConsoleScreen):
 
 	@staticmethod
 	def createWindow (h, w, y, x, title = None, refWin = None, initial_content = None, bkgd = None):
-		WinBox = curses.newwin (h, w, y, x)
-		WinBox.box ()
-		if title is not None:
-			WinBox.addstr (0, 2, '[' + title + ']')
-		WinBox.noutrefresh ()
-
 		if refWin is None:
-			Win = AWindow (h-2, w-2, y+1, x+1, initial_content = initial_content, bkgd = bkgd)
-			Win.refresh ()
+			Win = cls.ABoxWindow (h, w, y, x, title = title, initial_content = initial_content, bkgd = bkgd)
 		else:
 			Win = refWin
-			Win.win.resize (h-2, w-2)
-			Win.win.mvwin  (y+1, x+1)
-			Win.restore ()
+			Win.resize (h, w, y, x)
 
 		return Win 
 
@@ -134,7 +86,7 @@ class UserInterface(cls.ConsoleScreen):
 		if self.tltWin == None or resize:
 			h = 2 + 2
 			self.tltWin = self.createWindow (h, self.maxX, 0, 0, refWin = self.tltWin if (self.tltWin is not None) and resize else None, bkgd = curses.color_pair (3), initial_content = self.title)
-		
+
 	def showTip (self, resize = False):
 		if self.tipWin == None or resize:
 			h = 4 + 2
@@ -154,11 +106,8 @@ class UserInterface(cls.ConsoleScreen):
 
 	def print_on_window (self, window, *args, **kwargs):
 		if self.enabled:
-			self.clsLock.acquire ()
 			window.addstr (*args, **kwargs)
-			self.clsLock.release ()
 		else:
-			#print (args[0])
 			pass
 
 	def printEva (self, text, **kwargs):
@@ -198,12 +147,6 @@ def main (stdscr):
 		sleep (0.5)
 		ui.printEva ("222222222222222222222222222222222222222222222222222222222222222222222222222222222222222")
 		sleep (0.5)
-
-	try:
-		while True:
-			pass
-	except KeyboardInterrupt:
-		pass
 
 	ui.end ()
 
